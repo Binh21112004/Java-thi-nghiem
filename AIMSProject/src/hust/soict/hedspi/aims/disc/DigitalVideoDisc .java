e4b513e4f117e780324666a614e35d72528e6d12
Binh21112004 <167336354+Binh21112004@users.noreply.github.com>
2024-11-26 01:06:53 +0700

    Update DigitalVideoDisc .java

--- AIMSProject/src/hust/soict/hedspi/aims/disc/DigitalVideoDisc .java
+++ AIMSProject/src/hust/soict/hedspi/aims/disc/DigitalVideoDisc .java
@@ -6,21 +6,19 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
-    private int id; // Instance attribute ID
-    private static int nbDigitalVideoDiscs = 0; //  khai bao Class attribute 
-    
+   
      // constructor by title
     public DigitalVideoDisc(String title) {
           super();
         this.title = title;
-        this.id = ++nbDigitalVideoDiscs; // Update class variable and assign id
+       
     }
      // constructor by title,category,cost
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDiscs;
+      
     }
     // constructor by title, category,director,cost
     public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -28,7 +26,7 @@ public class DigitalVideoDisc {
         this.category = category;
         this.director = director;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDiscs;
+    
     }
      
     // constructor by all attributes
@@ -39,7 +37,7 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDiscs;
+     
     }
     
     
@@ -68,11 +66,7 @@ public class DigitalVideoDisc {
         return cost;
     }
     
-    public int getId() { 
-        return id; 
-    } // Ham lay id
-    
-    
+   
     // 
      public void setTitle(String titleSet){ title = titleSet; }
     public void setCategory(String categorySet){ category = categorySet; }
