7849cc9b8f6a045897b9097ef157010de8091c78
Binh21112004 <167336354+Binh21112004@users.noreply.github.com>
2024-12-03 05:21:06 +0700

    Update CompactDisc.java

--- AIMSProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
+++ AIMSProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -1,17 +1,22 @@
-package  hust.soict.hedspi.aims.media;
-import java.util.ArrayList;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hust.soict.hedspi.aims.media;
 
-public class CompactDisc extends Media {
+import java.util.*;
+
+public class CompactDisc extends Media implements Playable {
 
     private String artist;
-    private ArrayList<Track> tracks; 
+    private List<Track> tracks = new ArrayList<Track>();; 
 
-    
+    // Getter method
     public String getArtist() {
         return artist;
     }
 
-
+    // Constructor 
     public CompactDisc(String title) {
         super(title);
     }
@@ -20,9 +25,11 @@ public class CompactDisc extends Media {
         this.artist = artist;
     }
 
+    // Add and remove track
     public void addTrack(Track track) {
         if (!tracks.contains(track)) {
             tracks.add(track);
+            System.out.println("Track: " + track.getTitle() + " has been added to CD: " + this.getTitle());
         } else {
             System.out.println("Track already exists in CD.");
         }
@@ -31,11 +38,13 @@ public class CompactDisc extends Media {
     public void removeTrack(Track track) {
         if (tracks.contains(track)) {
             tracks.remove(track);
+            System.out.println("Track: " +track.getTitle() + " has been removed from CD: " + this.getTitle());
         } else {
             System.out.println("Track does not exist in CD.");
         }
     }
     
+    // Get length of the track
     public int getLength() {
         int totalLength = 0;
         for (Track track : tracks) {
@@ -44,4 +53,22 @@ public class CompactDisc extends Media {
         return totalLength;
     }
 
+    // Play method
+    @Override
+    public void play() {
+        System.out.println("Playing CD: " + this.getTitle());
+        System.out.println("CD length: " + this.getLength());
+        for (Track track : tracks) {
+            track.play();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getId() + " - CD: " + this.getTitle() +
+                " - Category: " + this.getCategory() +
+                " - Artist" + this.getArtist() +
+                " - Length: " + this.getLength() + " seconds" + 
+                " - Cost: " + this.getCost() + "$";
+    }
 }
